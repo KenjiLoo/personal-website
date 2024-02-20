9181c97b09155c833d5f02ffff2eac7df3429d3e
KenjiLoo <kenji@skyez.net>
2024-02-20 16:26:56 +0800

    Change meta image

--- layouts/default.vue
+++ layouts/default.vue
@@ -13,7 +13,7 @@ useHead({
       { charset: 'utf-8' },
       { name: 'description', content: "Kenji's Personal Site." },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { name: 'og:image', content: 'https://v2.amp-cdn.net/images/480468a77ad0a1a6b1b2e773d9371249e13e3ca2aff757' }
+      { name: 'og:image', content: '/images/avatar.png' }
     ],
   link: [
     {
